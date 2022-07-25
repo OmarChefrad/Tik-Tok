5714028ca3abea4a9f7c1bb71192b5eb08ceddd2
Omar <omarkiojoo@gmail.com>
2022-07-25 16:54:39 +0100

    comments

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -32,15 +32,15 @@ const Comments = ({
   const { allUsers, userProfile }: any = useAuthStore()
 
   return (
-    <div className="border-t-2 border-gray-200 pt-4 px-10 bg-[#F8F8F8] border-b-2 lg:pb-0 pb-[100px]">
-      <div className="overflow-scroll lg:h-[435px]">
+    <div className="border-t-2 border-gray-200 pt-4 px-10 mt-4 bg-[#F8F8F8] border-b-2 lg:pb-0 pb-[100px]">
+      <div className="overflow-scroll lg:h-[457px]">
         {comments?.length > 0 ? (
           comments?.map((item: IComment, idx: number) => (
             <>
               {allUsers?.map(
                 (user: IUser) =>
                   user._id === (item.postedBy._ref || item.postedBy._id) && (
-                    <div className=" p-2 items-center" key={idx}>
+                    <div className=" p-2 items-center" key={user._id}>
                       <Link href={`/profile/${user._id}`}>
                         <div className="flex items-start gap-3">
                           <div className="w-12 h-12">
@@ -83,18 +83,8 @@ const Comments = ({
               className="bg-primary px-6 py-4 text-md font-medium border-2 w-[250px] md:w-[700px] lg:w-[350px] border-gray-100 focus:outline-none focus:border-2 focus:border-gray-300 flex-1 rounded-lg"
               placeholder="Add comment.."
             />
-            <button
-              onClick={addComment}
-              className="relative inline-flex items-center justify-center p-4 px-5 py-3 overflow-hidden font-medium text-indigo-600 rounded-lg shadow-2xl group"
-            >
-              <span className="absolute top-0 left-0 w-40 h-40 -mt-10 -ml-3 transition-all duration-700 bg-rose-400 rounded-full blur-md ease"></span>
-              <span className="absolute inset-0 w-full h-full transition duration-700 group-hover:rotate-180 ease">
-                <span className="absolute bottom-0 left-0 w-24 h-24 -ml-10 bg-red-400 rounded-full blur-md"></span>
-                <span className="absolute bottom-0 right-0 w-24 h-24 -mr-10 bg-silentred rounded-full blur-md"></span>
-              </span>
-              <span className="relative text-white">
-                {isPostingComment ? "Commenting..." : "Comment"}
-              </span>
+            <button className="text-md text-gray-400 " onClick={addComment}>
+              {isPostingComment ? "Commenting..." : "Comment"}
             </button>
           </form>
         </div>
