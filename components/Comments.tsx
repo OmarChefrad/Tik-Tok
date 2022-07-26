112fb271bfd29a6b880333199692725d5dc83645
Omar <omarkiojoo@gmail.com>
2022-07-26 08:45:22 +0100

    liitle-bug

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -83,8 +83,18 @@ const Comments = ({
               className="bg-primary px-6 py-4 text-md font-medium border-2 w-[250px] md:w-[700px] lg:w-[350px] border-gray-100 focus:outline-none focus:border-2 focus:border-gray-300 flex-1 rounded-lg"
               placeholder="Add comment.."
             />
-            <button className="text-md text-gray-400 " onClick={addComment}>
-              {isPostingComment ? "Commenting..." : "Comment"}
+            <button
+              onClick={addComment}
+              className="relative inline-flex items-center justify-center p-4 px-5 py-3 overflow-hidden font-medium text-indigo-600 rounded-lg group shadow-md shadow-slate-400"
+            >
+              <span className="absolute top-0 left-0 w-40 h-40 -mt-10 -ml-3 transition-all duration-700 bg-rose-400 rounded-full blur-md ease"></span>
+              <span className="absolute inset-0 w-full h-full transition duration-700 group-hover:rotate-180 ease">
+                <span className="absolute bottom-0 left-0 w-24 h-28 -ml-10 bg-red-400 rounded-full blur-md"></span>
+                <span className="absolute bottom-0 right-0 w-24 h-24 -mr-10 bg-silentred rounded-full blur-md"></span>
+              </span>
+              <span className="relative text-white">
+                {isPostingComment ? "Commenting" : "Comment"}
+              </span>
             </button>
           </form>
         </div>
