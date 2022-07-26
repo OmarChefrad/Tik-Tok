587aedd12ac3d0fad6f4ef2479ad151e5af841fa
Omar <omarkiojoo@gmail.com>
2022-07-26 09:01:11 +0100

    input bar

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -80,7 +80,7 @@ const Comments = ({
             <input
               value={comment}
               onChange={(e) => setComment(e.target.value.trim())}
-              className="bg-primary ml-3 pl-2 py-4 text-md font-medium border-2 w-[227px] md:w-[700px] lg:w-[350px] border-gray-100 focus:outline-none focus:border-2 focus:border-gray-300 flex-1 rounded-lg"
+              className="bg-primary ml-3 pl-2 py-4 text-md font-medium border-2 w-[210px] md:w-[700px] lg:w-[350px] border-gray-100 focus:outline-none focus:border-2 focus:border-gray-300 flex-1 rounded-lg"
               placeholder="Add comment.."
             />
             <button
